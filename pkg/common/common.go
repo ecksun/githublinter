9f3f1415d6fcc6ea967d67fa7557befdb0d507de
ecksun <ecksun@gmail.com>
2019-11-06 00:30:09 +0100

    Remove unused method

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,20 +1,7 @@
 package common
 
-import (
-	"fmt"
-	"strings"
-)
-
 type GraphQLComment struct {
 	Path     string
 	Position int
 	Body     string
-}
-
-func (c GraphQLComment) String() string {
-	return fmt.Sprintf(`{
-    path: "%s"
-    position: %d
-    body: "%s"
-}`, c.Path, c.Position, strings.ReplaceAll(c.Body, "\"", "\\\""))
 }
